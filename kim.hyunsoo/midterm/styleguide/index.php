1b426710a99682562235dd0430bc621988b0d677
gitest01 <gustn9999@github.com>
2020-10-26 18:10:34 -0700

    php

--- kim.hyunsoo/midterm/styleguide/index.php
+++ kim.hyunsoo/midterm/styleguide/index.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Home</title>
+   <title>Store</title>
 
-   <?php include "../parts/meta.php"; ?>
-</head>
+   <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <?php include "parts/meta.php" ?>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script></head>
 <body>
    
-   <?php include "../parts/navbar.php"; ?>
-
-   
-   <!-- header>h1+p -->
-   
+   <?php include "parts/navbar.php" ?>
 
 
+   <div class="view-window" style="background-image:url(img/mountains001.jpg)">
+      <h2>Product Name</h2>
+   </div>
+   <div class="container">
+      <div class="card soft">
+         <h2>Welcome to the Store</h2>
+      </div>
+   </div>
 
-   <?php include "../parts/footer.php"; ?>
 </body>
 </html>
