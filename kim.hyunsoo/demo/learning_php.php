7ebe8cd8069bd23964a129981db1a6f59ccee80a
gitest01 <gustn9999@github.com>
2020-11-01 18:02:50 -0800

    admin

--- kim.hyunsoo/demo/learning_php.php
+++ kim.hyunsoo/demo/learning_php.php
@@ -1,17 +1,18 @@
 <?php
 
 // hello;
-// phpinfo ();
+// phpinfo();
 
-echo "<h1>Hello World<h1>";
+echo "<h1>Hello World</h1>";
 echo "<div>Goodbye World</div>";
 
 $a = 5;
 
-//String Interpolation
+// String Interpolation
 echo "<div> I have $a things</div>";
 echo '<div> I have $a things</div>';
 
+// VALUE TYPES
 
 // Number
 // Integer
@@ -20,6 +21,7 @@ $b = 15;
 $b = 0.576;
 
 $b = 10;
+
 
 // String
 $name = "Yerguy";
@@ -30,21 +32,20 @@ $isOn = true;
 
 // function, class, object
 
+
+
 // Math
 
-echo (5+4)*2;
+// Order of Operation
+// PEMDAS
+echo (5 + 4) * 2;
 
 // Concatenation
-
-echo "<div>$b + $a = " . ($b+$a) . "</div>";
 echo "<div>b + a = " . "c</div>";
+echo "<div>$b + $a = " . ($b+$a) . "</div>";
 
-// Name
-$firstname = "Hyunsoo";
-$lastname = 'Kim';
-
-$fullname = "Hyunsoo Kim"
 ?>
+
 
 <hr>
 <div>This is my name</div>
@@ -52,13 +53,13 @@ $fullname = "Hyunsoo Kim"
 
 <?php
 
-$firstname = 'Hyunsoo';
-$lastname = 'Kim' ;
-$fullname = $firstname. " " .$lastname;
+$firstname = 'Hamilton';
+$lastname = 'Cline';
+$fullname = "$firstname $lastname";
 
 echo $fullname;
 
-?>	
+?>
 
 </div>
 
@@ -69,39 +70,49 @@ echo $fullname;
 // Superglobal
 
 // ?name=Joey
+echo "<div><a href='?name=Joey'>Joey</a></div>";
+echo "<div><a href='?name=Frank'>Frank</a></div>";
+echo "<div>May name is {$_GET['name']}</div>";
 
-echo "<div><a href='?name=Joey'>Joey</a>My name is {$_GET['name']}</div>";
 
-phpinfo()
+echo "<div><a href='?name={$_GET['name']}&type=h1'>H1</a></div>";
+echo "<div><a href='?name={$_GET['name']}&type=button'>BUTTON</a></div>";
+echo "<{$_GET['type']}>May name is {$_GET['name']}</{$_GET['type']}>";
 
-//Arrays
+
+?>
+
+<hr>
+
+<?php
+
+// Arrays
 $colors = array("red","green","blue");
 $colors = ["red","green","blue"];
 
 echo $colors[1];
 
 echo "
-	<br>$colors[0]
-	<br>$colors[1]
-	<br>$colors[2]
-	";
+   <br>$colors[0]
+   <br>$colors[1]
+   <br>$colors[2]
+";
 
 echo count($colors);
 
 ?>
 
-<div style="color:<?= $colors[1]?>">
-	This text is green
-	
+<div style="color:<?= $colors[1] ?>">
+   This text is green
 </div>
 
 <?php
 
 // Associative Array
 $colorsAssociative = [
-  "red" => "#f00",
-  "green" => "#0f0",
-  "blue" => "#00f",
+   "red" => "#f00",
+   "green" => "#0f0",
+   "blue" => "#00f"
 ];
 
 echo $colorsAssociative['red'];
@@ -114,22 +125,23 @@ echo $colorsAssociative['red'];
 
 // Casting
 $c = "$a";
-$d = $c*1; 
+$d = $c*1;
 
 $colorsObject = (object)$colorsAssociative;
 
 // echo $colorsObject;
 
+
 echo "<hr>";
 
-//Array Index Notation
+// Array Index Notation
 echo $colors[0]."<br>";
 echo $colorsAssociative['red']."<br>";
-
+echo $colorsAssociative[$colors[0]]."<br>";
 
 // Object Property Notation
 echo $colorsObject->red."<br>";
-echo $colorsObject ->{$colors[0]}."<br>";
+echo $colorsObject->{$colors[0]}."<br>";
 
 ?>
 
@@ -138,4 +150,16 @@ echo $colorsObject ->{$colors[0]}."<br>";
 <?php
 
 print_r($colors);
+echo "<hr>";
+print_r($colorsAssociative);
+echo "<hr>";
+echo "<pre>",print_r($colorsObject),"</pre>";
 
+
+// CUSTOM FUNCTIONS
+function print_p($d) {
+   echo "<pre>",print_r($d),"</pre>";
+}
+
+print_p($_GET);
+print_p($colors);
---
+++ kim.hyunsoo/demo/users_admin.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>User Administrator</title>
+
+   <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<base href="/aau/wnm608/kim.hyunsoo/">
+
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="css/storetheme.css">
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script></head>
+<body>
+
+   <header class="navbar">
+      <div class="container display-flex">
+         <div class="flex-none">
+            <h1>Users Admin</h1>
+         </div>
+         <div class="flex-stretch"></div>
+         <!-- nav.nav.flex-none>ul>li>a[href=#]>{List} -->
+         <nav class="nav flex-none">
+            <ul>
+               <li><a href="/aau/wnm608/fernandes.shiny/demo/users_admin.php">List</a></li>
+            </ul>
+         </nav>
+      </div>
+   </header>
+
+   <div class="container">
+      <div class="card soft">
+
+         
+         <h2>User List</h2>
+
+         <ul>
+         <li>
+            <a href='/aau/wnm608/kim.hyunsoo/demo/users_admin.php?id=0'>Hamilton Cline</a>
+            </li><li>
+            <a href='/aau/wnm608/kim.hyunsoo/demo/users_admin.php?id=1'>Phoebe Buffay</a>
+            </li><li>
+            <a href='/aau/wnm608/kim.hyunsoo/demo/users_admin.php?id=2'>Joey Tribbiani</a>
+            </li><li>
+            <a href='/aau/wnm608/kim.hyunsoo/demo/users_admin.php?id=3'>Chandler Bing</a>
+            </li><li>
+            <a href='/aau/wnm608/kim.hyunsoo/demo/users_admin.php?id=4'>Monica Gellar</a>
+            </li>         </ul>
+
+               </div>
+   </div>
+   
+</body>
+</html>
